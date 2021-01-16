6388968d1d2495475197c37b151d5096d53397a9
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-16 22:21:35 +0900

    if and for

--- src/List.js
+++ src/List.js
@@ -1,8 +1,18 @@
+const LANGUAGES = [
+  'JavaScirpt',
+  'PHP',
+  'Ruby'
+];
+
 export const List = ({ title }) => {
   return (
     <div>
       <h4>{ title }</h4>
-      <div>リストです</div>
+      {
+        LANGUAGES.map((lang, index) => {
+          return <div key={index}>{lang}</div>
+        })
+      }
     </div>
   )
 }
