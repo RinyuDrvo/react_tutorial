98a0d6fb588fc5dd8cac4219ac73e135165ba21f
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-16 18:40:44 +0900

    ステート変更メソッド作成

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { List } from "./List";
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示です');
+  }
+
   return (
     <div>
       { description }
       <List title="取扱言語一覧" />
+      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }
