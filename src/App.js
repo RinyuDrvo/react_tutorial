91c7b6e4f0efe2d906764c01f45432365f9a87cf
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-16 22:38:42 +0900

    if and for

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { List } from './List';
+import { Form } from './Form';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { description: 'クリック前の表示' }
+    this.state = {
+      tab: 'list'
+    }
   }
 
-  changeDescription() {
-    this.setState({
-      description: 'クリック後の表示です'
-    })
-  }
   render() {
-    const { description } = this.state;
+    const { tab, description } = this.state;
     return (
       <div>
-        { description }
-        <List title="取扱言語の一覧" />
-        <button onClick={() => this.changeDescription()}>ボタン</button>
+        <header>
+          <ul>
+            <li onClick={() => this.setState({tab: 'list'})}>リスト</li>
+            <li onClick={() => this.setState({tab: 'form'})}>フォーム</li>
+          </ul>
+        </header>
+        <hr/>
+        {
+          tab === 'list' ?
+            <List /> :
+            <Form />
+        }
       </div>
     )
   }
--- src/List.js
+++ src/List.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+const LANGUAGES = [
+  'JavaScirp',
+  'PHP',
+  'Ruby'
+];
+
 export class List extends React.Component {
   render() {
-    const { title } = this.props;
     return (
       <div>
-        <h4>{ title }</h4>
-        <div>リストです</div>
+        {
+          LANGUAGES.map((lang, index) => {
+            return <div key={index}>{lang}</div>
+          })
+        }
       </div>
     )
   }
