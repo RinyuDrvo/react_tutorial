b254f490ab6ad42db8858bba34286693a795a65c
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-17 17:50:03 +0900

    言語取得メソッド定義

--- src/const/languages.js
+++ src/const/languages.js
@@ -3,3 +3,11 @@ export const LANGUAGES = [
   'PHP',
   'Ruby'
 ];
+
+export const getLanguages = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(LANGUAGES);
+    }, 1000)
+  })
+}
