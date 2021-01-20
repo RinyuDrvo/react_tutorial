a19bf4310a4405a5d0107815686ac2dff8ac3971
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-20 09:31:07 +0900

    style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { List } from './List';
 import { Form } from './Form';
 import { getLanguages } from './const/langages';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  displey: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #E0E0E0;
+`
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${props => props.focused ? '2px solid #F44336'  : 'none'}
+`
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -35,13 +57,12 @@ class App extends React.Component {
     const { tab, langs } = this.state;
     return (
       <div>
-        <header>
-          <ul>
-            <li onClick={() => this.setState({tab: 'list'})}>リスト</li>
-            <li onClick={() => this.setState({tab: 'form'})}>フォーム</li>
-          </ul>
-        </header>
-        <hr/>
+        <Header>
+          <HeaderUl>
+            <HeaderLi focused={tab === 'list'} onClick={() => this.setState({tab: 'list'})}>リスト</HeaderLi>
+            <HeaderLi focused={tab === 'form'} onClick={() => this.setState({tab: 'form'})}>フォーム</HeaderLi>
+          </HeaderUl>
+        </Header>
         {
           tab === 'list' ?
             <List langs={langs} /> :
