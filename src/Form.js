833f7cd12d3e245d8df7436947b0cce801821326
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-27 09:04:24 +0900

    use tab body container

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from './components/button';
-
-const Container = styled.div`
-  padding: 12px 64px;
-`
+import { TabBodyContainer } from './components/tab-body-container';
 
 const Label = styled.label`
   display: flex;
@@ -41,7 +38,7 @@ export class Form extends React.Component {
   render() {
     const { text } = this.state;
     return (
-      <Container>
+      <TabBodyContainer title="新しい言語の追加">
         <h4>新しい言語を追加</h4>
         <form onSubmit={(e) => this.submitForm(e)}>
           <div>
@@ -56,7 +53,7 @@ export class Form extends React.Component {
             <FormButton>追加</FormButton>
           </ButtonContainer>
         </form>
-      </Container>
+      </TabBodyContainer>
     )
   }
 }
