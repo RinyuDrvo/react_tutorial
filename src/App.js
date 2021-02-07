be8e74973815f83fb5c33063846c5a9b8124b6a0
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-02-07 21:27:28 +0900

    make header components

--- src/App.js
+++ src/App.js
@@ -2,28 +2,8 @@ import React from 'react';
 import { List } from './List';
 import { Form } from './Form';
 import { getLanguages } from './const/langages';
-import styled from 'styled-components';
 import { withLoading } from './hoc/withLoading';
-
-const Header = styled.div`
-  displey: flex;
-  justify-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #E0E0E0;
-`
-
-const HeaderUl = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-
-const HeaderLi = styled.li`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${props => props.focused ? '2px solid #F44336'  : 'none'}
-`
+import { Header } from './Header';
 
 class App extends React.Component {
   constructor(props) {
@@ -46,12 +26,7 @@ class App extends React.Component {
     const { tab, langs } = this.state;
     return (
       <div>
-        <Header>
-          <HeaderUl>
-            <HeaderLi focused={tab === 'list'} onClick={() => this.setState({tab: 'list'})}>リスト</HeaderLi>
-            <HeaderLi focused={tab === 'form'} onClick={() => this.setState({tab: 'form'})}>フォーム</HeaderLi>
-          </HeaderUl>
-        </Header>
+        <Header tab={tab} setTab={(t) => this.setState({ tab: t })} />
         {
           tab === 'list' ?
             <List langs={langs} /> :
