50a9b0806317a60958da3484f170f6f84017c45c
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-02-07 18:30:43 +0900

    make theme change with header button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
 import { Header } from './Header';
+import { ThemeContext } from './contexts/ThemeContext';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+  color: ${({ theme }) => theme.color};
+  background-color: ${({ theme }) => theme.backgroundColor};
+`
 
 function App({ data }) {
   const [tab, setTab] = useState('list');
   const [langs, setLangs] = useState(data);
+  const [theme] = useContext(ThemeContext);
 
   // 言語セット
   const addLang = (lang) => {
@@ -14,12 +23,12 @@ function App({ data }) {
   }
 
   return (
-    <div>
+    <Container theme={theme}>
       <Header tab={tab} setTab={setTab} />
       {
         tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang} />
       }
-    </div>
+    </Container>
   );
 }
 
